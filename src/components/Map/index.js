4a81a7eb36a56b126b4aa44193b64b2b1a27578d
wuyaheng <wuyaheng2016@gmail.com>
2021-03-07 11:14:29 -0500

    add different color scale based on confirmed cases

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -35,14 +35,33 @@ export default (props) => {
       props.pins.forEach((pin) =>
       (pin?.properties?.covid?.lat && pin?.properties?.covid?.long) ? 
       L.marker([pin.properties.covid.lat, pin.properties.covid.long]).addTo(confirmMap).bindTooltip('<p><b>' + pin.properties.ADMIN + 
-      '</b></p><p><b>Recovered: </b>' + pin.properties.covid.recovered + '</p><p><b>Confirmed:</b> ' + pin.properties.covid.confirmed + '</p><p><b>Death: </b>' + pin.properties.covid.deaths + '</p><p><b>Last Update: </b>' + moment(pin.properties.covid.lastUpdate).format('L') + '</p>') 
+      '</b></p><p><b>Confirmed:</b> ' + pin.properties.covid.confirmed + '</p><p><b>Death: </b>' + pin.properties.covid.deaths + '</p><p><b>Last Update: </b>' + moment(pin.properties.covid.lastUpdate).format('L') + '</p>') 
    : null );
+
+
+      function chooseColor(x) {
+        if (x >= 900000) {
+          return "#641220"
+        } else if (x >= 50000) {
+          return "#6e1423";
+        } else if (x >= 1000) {
+          return "#85182a";
+        } else if (x >= 100) {
+          return "#a11d33";
+        } else if (x >= 10) {
+          return "#a71e34";
+        } else {
+          return "black";
+        }
+    }
+
 
                 var geoJson = L.geoJson(props.pins, {
                 style: function(feature) {
+                  console.log(feature.properties.covid?.confirmed)
                     return {
                     color: "white",
-                    fillColor: "lightblue",
+                    fillColor: chooseColor(feature?.properties?.covid?.confirmed),
                     fillOpacity: 0.7,
                     weight: 1.5
                     };
@@ -62,7 +81,7 @@ export default (props) => {
                       confirmMap.fitBounds(event.target.getBounds()); 
                     }
                     });
-                    layer.bindTooltip("<p><b>" + feature.properties.ADMIN + "</b></p>");
+                    layer.bindTooltip("<p><b>" + feature.properties.ADMIN + "</b></p><p><b>Confirmed:</b> " + feature?.properties?.covid?.confirmed + "</p><p><b>Deaths: </b>" + feature?.properties?.covid?.deaths + "</p><p><b>Last Update: </b>" + moment(feature?.properties?.covid?.lastUpdate).format('L') + "</p>");
                 }
                 }).addTo(confirmMap);
     }
